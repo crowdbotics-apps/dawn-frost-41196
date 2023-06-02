dad01fbc2bb757502e33b517164820d56e451e9a
Anand Kulkarni <anand@crowdbotics.com>
2023-06-02 14:36:19 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "dawn-frost-41196",
   url: "https://dawn-frost-41196.botics.co",
   api: "https://dawn-frost-41196.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
